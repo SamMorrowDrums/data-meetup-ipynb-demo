996a5e050c1401a80a58b6f414a4b4d9325170af
Sam Morrow <sammorrowdrums@github.com>
2024-04-09 14:56:43 +0200

    add a box and whisker example plot

--- src/example.ipynb.py
+++ src/example.ipynb.py
@@ -6,3 +6,23 @@
 
 print("Hello, World!")
 
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+
+# Assuming you have a list of data called 'data'
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Generate the box and whisker plot
+plt.boxplot(data)
+
+# Add labels and title
+plt.xlabel('Data')
+plt.ylabel('Values')
+plt.title('Box and Whisker Plot')
+
+# Show the plot
+plt.show()
+
